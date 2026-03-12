55f3d4ad018a92c051bc9949d0e57145c3a4f7f8
JimmiesAndTheCoders <123126208+JimmiesAndTheCoders@users.noreply.github.com>
2026-03-11 20:00:22 -0400

    Looks like it's organizing time!

--- build.bat
+++ build.bat
@@ -1,31 +1,43 @@
 @echo off
-
 SET RAYLIB_PATH=C:\raylib\raylib
 SET COMPILER_PATH=C:\raylib\w64devkit\bin
 SET PATH=%COMPILER_PATH%;%PATH%
 
-:: Target Directory for Windows
 SET BUILD_DIR=build\windows
 
 echo Building Super Scratch Kart R for Windows...
 
-:: Create specific platform directory if it doesn't exist
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
 
-:: --- ASSET SYNC ---
 if exist "assets" (
     echo Syncing assets to %BUILD_DIR%...
     xcopy /E /I /Y "assets" "%BUILD_DIR%\assets"
 )
 
-:: Compile and output to the platform directory
-g++ src/*.cpp -o %BUILD_DIR%\SuperScratchKartR.exe -O2 -Wall -I%RAYLIB_PATH%\src -L%RAYLIB_PATH%\src -Iinclude -lraylib -lopengl32 -lgdi32 -lwinmm
+setlocal enabledelayedexpansion
+set "SOURCES="
+set "INCLUDES=-Isrc"
+
+for /r "src" %%f in (*.cpp) do (
+    set "SOURCES=!SOURCES! "%%f""
+)
+
+for /d /r "src" %%d in (*) do (
+    set "INCLUDES=!INCLUDES! -I"%%d""
+)
+
+echo Compiling...
+g++ %SOURCES% -o %BUILD_DIR%\SuperScratchKartR.exe -O2 -Wall ^
+    -I%RAYLIB_PATH%\src ^
+    -L%RAYLIB_PATH%\src ^
+    %INCLUDES% ^
+    -lraylib -lopengl32 -lgdi32 -lwinmm
 
 if %ERRORLEVEL% EQU 0 (
     echo Build Successful! Running game...
-    cd %BUILD_DIR%
+    pushd %BUILD_DIR%
     SuperScratchKartR.exe
-    cd ..\..
+    popd
 ) else (
     echo Build Failed.
     pause
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Detect OS for directory naming
 if [[ "$OSTYPE" == "darwin"* ]]; then
     OS_NAME="macos"
 else
@@ -8,32 +7,28 @@ else
 fi
 
 BUILD_DIR="build/$OS_NAME"
-
 echo "Building Super Scratch Kart R for $OS_NAME..."
 
-# Create platform directory
 mkdir -p "$BUILD_DIR"
 
-# --- ASSET SYNC ---
 if [ -d "assets" ]; then
     echo "Syncing assets to $BUILD_DIR..."
     cp -r assets "$BUILD_DIR/"
 fi
 
-# Compile based on OS
+SOURCES=$(find src -name "*.cpp")
+
+FLAGS="-O2 -Wall -Isrc"
+
 if [ "$OS_NAME" == "macos" ]; then
-    # macOS build command
-    g++ src/*.cpp -Iinclude -o "$BUILD_DIR/SuperScratchKartR" -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
+    g++ $SOURCES $FLAGS -o "$BUILD_DIR/SuperScratchKartR" -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
 else
-    # Linux build command
-    g++ src/*.cpp -Iinclude -o "$BUILD_DIR/SuperScratchKartR" -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+    g++ $SOURCES $FLAGS -o "$BUILD_DIR/SuperScratchKartR" -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 fi
 
 if [ $? -eq 0 ]; then
     echo "Build Successful! Running game..."
-    cd "$BUILD_DIR"
-    ./SuperScratchKartR
-    cd ../..
+    cd "$BUILD_DIR" && ./SuperScratchKartR
 else
     echo "Build Failed."
 fi
